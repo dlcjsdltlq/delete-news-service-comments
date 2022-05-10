0f5503e76a9d547d2ab2caf2f9aedd03bd1ecf8e
dlcjsdltlq <70802559+dlcjsdltlq@users.noreply.github.com>
2022-05-10 14:55:52 +0900

    fix typo

--- daum.js
+++ daum.js
@@ -1,19 +1,19 @@
-let commentLists = [];
-const getCommentLists = () =>
+let commentList = [];
+const getCommentList= () =>
 	[...document.querySelectorAll('#tabcontent > div > ul > li')].forEach((e) => {
-		if (e.querySelector('div > button.tooltip')) commentLists.push(e.id);
+		if (e.querySelector('div > button.tooltip')) commentList.push(e.id);
 	});
 window.confirm = () => {
 	console.log('OK');
 	return true;
 };
-getCommentLists();
+getCommentList();
 setInterval(() => {
-	if (!commentLists.length) {
+	if (!commentList.length) {
 		document.querySelector('#tabcontent > div > div > button').click();
-		getCommentLists();
+		getCommentList();
 	}
-	const id = commentLists.shift();
+	const id = commentList.shift();
 	try {
 		document.querySelector(`#${id} > div > button.tooltip`).click();
 		document.querySelector(`#${id} > div > span.tooltip_box > a.link_delete`).click();
