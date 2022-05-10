03f3b9be1f5451bb03bd94ea94dd7a17a6eb342d
dlcjsdltlq <70802559+dlcjsdltlq@users.noreply.github.com>
2022-05-10 14:57:05 +0900

    fix typo

--- naver.js
+++ naver.js
@@ -1,15 +1,15 @@
-let commentLists = [];
+let commentList = [];
 window.confirm = () => {
     console.log('OK');
     return true;
 }
-const getCommentLists = () => commentLists = [...document.querySelectorAll('#cbox_module_wai_u_cbox_content_wrap_tabpanel > ul > li > div > div > div.u_cbox_info > span > span > span > span > a.u_cbox_btn_delete')];
-getCommentLists();
+const getCommentList = () => commentList = [...document.querySelectorAll('#cbox_module_wai_u_cbox_content_wrap_tabpanel > ul > li > div > div > div.u_cbox_info > span > span > span > span > a.u_cbox_btn_delete')];
+getCommentList();
 setInterval(() => {
-    if (!commentLists.length) {
+    if (!commentList.length) {
         document.querySelector('body > div.u_cbox.u_cbox_layer_wrap > div > div.u_cbox_layer_user.u_cbox_layer_user_v2 > div > div > div.u_cbox_user_cont > div.u_cbox_paginate > a').click();
-        getCommentLists();
+        getCommentList();
     }
-    const element = commentLists.shift();
+    const element = commentList.shift();
     element.click();
 }, 500)
